af69aad1967949b6f527fd66c2a80e7f6fe2f293
wsw2025 <shuwenw2025@student.cis.edu.hk>
2024-04-23 15:08:04 +0800

    .

--- cordova-wrapper/www/js/profile.js
+++ cordova-wrapper/www/js/profile.js
@@ -5,23 +5,27 @@ import { collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.1.1/fi
 document.addEventListener('DOMContentLoaded', () => {
   const storage = getStorage(); // Initialize Firebase Storage instance
 
-  getDocs(collection(db, 'items'))
+  getDocs(collection(db, 'listings'))
     .then((querySnapshot) => {
-      const itemList = document.getElementById('recommendation'); // Get the container element
+      const itemList = document.getElementById('listing-content'); // Get the container element
       let i = 0;
       querySnapshot.forEach((doc) => {
         const data = doc.data();
-        const imageRef = storageRef(storage, `${data.id}.png`); // Construct the reference to the image file
+        const imageRef = storageRef(storage, `listings/${data.id}.png`); // Construct the reference to the image file
 
         getDownloadURL(imageRef) // Fetch the URL for the image
           .then((url) => {
             const div = document.createElement('div'); // Create a new div element
             div.innerHTML = `
-              <div style="width: 110px; height: 177px; padding-bottom: 25px; left: ${Math.floor(i % 3) * 126}px; top: ${Math.floor(i / 3) * 170 + 45}px; position: absolute;">
-                <div style="width: 110px; height: 110px; background-image: url('${url}'); background-size: cover; background-position: center center; border-radius: 8px;"></div>
-                  <div style="top: 120px;position: absolute; width: 110px; color: black; font-size: 14px; font-family: Inter; font-weight: 600; word-wrap: break-word">${data.name}</div>
-                  <div style="position:relative; top:32px;width: 110px; color: black; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">${data.quantity}</div>
-           </div>
+
+<div style="width: 343px; height: 77px; left: 0; top: 20px; position: relative">
+        <div style="width: 50px; height: 50px; background-image: url(${url})left: 0px; top: ${Math.floor(i*1)}px; position: absolute; background: #F6F6F6; border-radius: 8px"></div>
+        <div style="left: 66px; top: 0px; position: absolute; color: black; font-size: 16px; font-family: Inter; font-weight: 700; word-wrap: break-word">${data.name}</div>
+        <div style="left: 293px; top: 2px; position: absolute; text-align: right; color: #BDBDBD; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">${data.timestamp}min ago</div>
+        <div style="width: 269px; left: 66px; top: 27px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">${data.desc}</div>
+        <div style="width: 277px; height: 0px; left: 66px; top: 77px; position: absolute; border: 1px #E8E8E8 solid"></div>
+      </div>
+
 
             `;
             itemList.appendChild(div); // Append the div to the container element
